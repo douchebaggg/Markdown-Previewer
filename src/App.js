a51d063f5aeb8c90871ba5b0d95f859b3e1cd9d1
Jarudet Sangprapai <49513867+douchebaggg@users.noreply.github.com>
2023-09-17 09:59:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { marked } from 'marked';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
 function App() {
+  const [markdownText, setMarkdown] = useState(`
+  # Welcome to my React Markdown Previewer!
+  
+  ## This is a sub-heading...
+  
+  ### And here's some other cool stuff:
+
+  [This is a link](https://example.com)
+  
+  \`inline code\`
+  
+  \`\`\`
+  // Code block
+  function example() {
+    console.log('Hello, world!');
+  }
+  \`\`\`
+  
+  - List item 1
+  - List item 2
+  
+  > This is a blockquote
+  
+  ![Freecodecamp logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  
+  **Bold text**
+  `);
+  marked.setOptions({breaks: true});
+  const [isShown, setIsShown] = useState(true);
+  const handleClick = event => {
+    setIsShown(current => !current);
+  };
+  const [isPreview, setIsPreview] = useState(true);
+  const showPreview = event => {
+    setIsPreview(current => !current)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className= {isPreview ? 'editor-container' : 'editor-container hide'}>
+        <div className="toolbar"><FontAwesomeIcon icon={faFreeCodeCamp} style={{width:"30px",fontSize: "1.25rem"}} />Editor <button className='icon' onClick={handleClick}> <FontAwesomeIcon className='icon' icon={faXmark} /></button>
+        </div>
+        <textarea id ="editor" type ="text" value ={markdownText} onChange={(event) => {setMarkdown(event.target.value)}} className={isShown ? '' : 'textarea-full-height'}></textarea >
+      </div>  
+    <div className= {isShown ? "preview-wrapper maximum" : "preview-wrapper hide" }>
+    <div className="toolbar"> <FontAwesomeIcon icon={faFreeCodeCamp} style={{width:"30px",fontSize: "1.25rem"}} /> Preview <button className='icon' onClick={showPreview}><FontAwesomeIcon className='icon' icon={faXmark} /></button> </div>
+      <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdownText),}}>
+      </div>
+    </div>
     </div>
   );
 }
-
 export default App;
